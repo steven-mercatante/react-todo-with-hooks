e9f9d203e9c7adfcd056787c2201c5e246fbab82
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-29 18:11:30 -0400

    build basic list & create functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          task={todo.task}
+          isComplete={todo.isComplete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function TodoItem({ id, task, isComplete }) {
+  const [_isComplete, setIsComplete] = useState(isComplete);
+
+  function handleChange(event) {
+    console.log(event.target.checked);
+    setIsComplete(event.target.checked);
+  }
+
+  return (
+    <li key={id} className="todo-item">
+      <label htmlFor="isComplete">
+        <span className={_isComplete ? "complete" : ""}>{task}</span>
+        <input
+          type="checkbox"
+          name="isComplete"
+          checked={_isComplete}
+          onChange={handleChange}
+        />
+      </label>
+    </li>
+  );
+}
+
+function TodoForm({ createTodo }) {
+  const [task, setTask] = useState("");
+
+  function handleChange(event) {
+    setTask(event.target.value);
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    createTodo(task);
+    setTask("");
+  }
+
+  return (
+    <form>
+      <label htmlFor="task">Task</label>
+      <input type="text" name="task" value={task} onChange={handleChange} />
+      <button onClick={handleClick}>Add</button>
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      task: "Start React project",
+      isComplete: true
+    },
+    {
+      id: 2,
+      task: "Cook dinner",
+      isComplete: false
+    }
+  ]);
+
+  function createTodo(task) {
+    const nextId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    const newTodo = {
+      id: nextId,
+      task: task
+    };
+
+    setTodos(todos.concat(newTodo));
+  }
+
+  function deleteTodo(id) {}
+
+  function toggleTodo(id) {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-app">
+        <TodoForm createTodo={createTodo} />
+        <TodoList todos={todos} />
+      </div>
     </div>
   );
 }
