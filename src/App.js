2da2069fe9b3aa1aea9d9e7bd5bbe81b18d4e23f
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-29 18:44:14 -0400

    flesh out functionality

--- src/App.js
+++ src/App.js
@@ -1,44 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function TodoList({ todos }) {
-  return (
-    <ul>
-      {todos.map(todo => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          task={todo.task}
-          isComplete={todo.isComplete}
-        />
-      ))}
-    </ul>
-  );
-}
-
-function TodoItem({ id, task, isComplete }) {
-  const [_isComplete, setIsComplete] = useState(isComplete);
-
-  function handleChange(event) {
-    console.log(event.target.checked);
-    setIsComplete(event.target.checked);
-  }
-
-  return (
-    <li key={id} className="todo-item">
-      <label htmlFor="isComplete">
-        <span className={_isComplete ? "complete" : ""}>{task}</span>
-        <input
-          type="checkbox"
-          name="isComplete"
-          checked={_isComplete}
-          onChange={handleChange}
-        />
-      </label>
-    </li>
-  );
-}
-
 function TodoForm({ createTodo }) {
   const [task, setTask] = useState("");
 
@@ -61,6 +23,29 @@ function TodoForm({ createTodo }) {
   );
 }
 
+function TodoItem({ id, task, isComplete, toggleTodo }) {
+  const [_isComplete, setIsComplete] = useState(isComplete);
+
+  function handleChange(event) {
+    setIsComplete(event.target.checked);
+    toggleTodo(id);
+  }
+
+  return (
+    <li key={id} className="todo-item">
+      <label>
+        <span className={isComplete ? "complete" : ""}>{task}</span>
+        <input
+          type="checkbox"
+          name="isComplete"
+          checked={_isComplete}
+          onChange={handleChange}
+        />
+      </label>
+    </li>
+  );
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -74,26 +59,71 @@ function App() {
       isComplete: false
     }
   ]);
+  const [showCompleted, setShowCompleted] = useState(true);
+
+  let visibleTodos;
+  if (showCompleted) {
+    visibleTodos = [...todos];
+  } else {
+    visibleTodos = todos.filter(todo => !todo.isComplete);
+  }
+
+  const nbCompleted = todos.reduce((acc, todo) => {
+    if (todo.isComplete) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
 
   function createTodo(task) {
-    const nextId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
     const newTodo = {
-      id: nextId,
+      id,
       task: task
     };
 
     setTodos(todos.concat(newTodo));
   }
 
-  function deleteTodo(id) {}
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.isComplete = !todo.isComplete;
+        }
+        return todo;
+      })
+    );
+  }
 
-  function toggleTodo(id) {}
+  function handleToggleVisiblity(event) {
+    setShowCompleted(event.target.checked);
+  }
 
   return (
     <div className="App">
       <div className="todo-app">
         <TodoForm createTodo={createTodo} />
-        <TodoList todos={todos} />
+        <label>
+          Show completed ({nbCompleted}):
+          <input
+            type="checkbox"
+            name="show-completed"
+            checked={showCompleted}
+            onChange={handleToggleVisiblity}
+          />
+        </label>
+        <ul>
+          {visibleTodos.reverse().map(todo => (
+            <TodoItem
+              key={todo.id}
+              id={todo.id}
+              task={todo.task}
+              isComplete={todo.isComplete}
+              toggleTodo={toggleTodo}
+            />
+          ))}
+        </ul>
       </div>
     </div>
   );
